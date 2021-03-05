effbe1846a062728594708bdbe3aedb182ac6083
Akira Kusumoto <akira.kusumoto@hennge.com>
2021-03-05 18:49:13 +0900

    add firebase settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+var config = {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyD80x2QS14PAriMg41JL2GJotKGuYXhivo",
+  authDomain: "kportal-83694.firebaseapp.com",
+  databaseURL: "https://kportal-83694.firebaseio.com",
+  projectId: "kportal-83694",
+  storageBucket: "kportal-83694.appspot.com",
+  messagingSenderId: "761049942089",
+  appId: "1:761049942089:web:679c0b991706722ac95798",
+  measurementId: "G-C0SCPBJMG8"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
